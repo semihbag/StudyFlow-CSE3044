0c62099c9ca0f9e006cab533413cedfb97c2197c
semihbag <semih702bag@gmail.com>
2024-04-27 22:24:13 +0300

    major pages created x2

--- app/src/main/java/com/example/studyflow/MainActivity.kt
+++ app/src/main/java/com/example/studyflow/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.studyflow
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun goFocusPage(view: View) {
+        val intent = Intent(applicationContext, FocusActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun goFlashMindPage(view: View) {
+        val intent = Intent(applicationContext, FlashMindActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun goPlanningPage(view: View) {
+        val intent = Intent(applicationContext, PlanningActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun goToDoPage(view: View) {
+        val intent = Intent(applicationContext, ToDoActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun goAnalysisPage(view: View) {
+        val intent = Intent(applicationContext, AnalysisActivity::class.java)
+        startActivity(intent)
+    }
+
 }
---
+++ app/src/main/java/com/example/studyflow/PlanningActivity.kt
@@ -0,0 +1,11 @@
+package com.example.studyflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlanningActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_planning)
+    }
+}
