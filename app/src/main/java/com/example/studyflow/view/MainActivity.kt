7126faead79c814c74eb64ec0ea4e490eb47b3e8
semihbag <semih702bag@gmail.com>
2024-04-30 13:09:03 +0300

    basis of mvvm

--- app/src/main/java/com/example/studyflow/view/MainActivity.kt
+++ app/src/main/java/com/example/studyflow/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.studyflow
+package com.example.studyflow.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.studyflow.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
