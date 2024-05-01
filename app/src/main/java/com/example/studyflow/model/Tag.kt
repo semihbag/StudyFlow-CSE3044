9a8553dec82a86c2e32dcc3ec669ca7ccd93c5c0
semihbag <semih702bag@gmail.com>
2024-05-01 14:32:55 +0300

    create tag model

---
+++ app/src/main/java/com/example/studyflow/model/Tag.kt
@@ -0,0 +1,12 @@
+package com.example.studyflow.model
+
+data class Tag (
+    val tagName: String,
+    val totalNumberOfPomodoro: Int = 0,
+    val totalNumberOfFocusedMinute: Int = 0,
+    val totalNumberOfOutOfFocusedMinute: Int = 0,
+    val totalNumberOfStop: Int = 0,
+    val totalNumberOfCard: Int = 0,
+    val totalNumberOfCurrentCorrectAnswer: Int = 0
+    ) {
+}
