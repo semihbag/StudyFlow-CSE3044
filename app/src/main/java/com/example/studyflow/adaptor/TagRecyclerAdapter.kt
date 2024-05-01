e123e6d9750c57d49829137f524f97469150a430
semihbag <semih702bag@gmail.com>
2024-05-01 21:24:06 +0300

    taglist adaptor implemented

---
+++ app/src/main/java/com/example/studyflow/adaptor/TagRecyclerAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.studyflow.adaptor
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.studyflow.R
+import com.example.studyflow.model.Tag
+
+class TagRecyclerAdapter(val tagList : ArrayList<Tag>) : RecyclerView.Adapter<TagRecyclerAdapter.TagViewHolder>() {
+    // create class
+    class TagViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    }
+
+    // override functions (came from RecyclerView.Adapter)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TagViewHolder {
+        val inflater =  LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.tag_row,parent,false)
+        return TagViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return tagList.size
+    }
+
+    override fun onBindViewHolder(holder: TagViewHolder, position: Int) {
+        holder.itemView.findViewById<TextView>(R.id.tagTittle).text = tagList[position].tagName
+        holder.itemView.findViewById<TextView>(R.id.totalPomodoro).text = tagList[position].totalNumberOfPomodoro.toString()
+        holder.itemView.findViewById<TextView>(R.id.totalCard).text = tagList[position].totalNumberOfCard.toString()
+        holder.itemView.findViewById<TextView>(R.id.totalFocused).text = tagList[position].totalNumberOfOutOfFocusedMinute.toString()
+        holder.itemView.findViewById<TextView>(R.id.totalOutOfFocused).text = tagList[position].totalNumberOfOutOfFocusedMinute.toString()
+        holder.itemView.findViewById<TextView>(R.id.totalStop).text = tagList[position].totalNumberOfStop.toString()
+
+        // calculate the ratio of correct answer
+        val numberOfCorrectAnswer = tagList[position].totalNumberOfCurrentCorrectAnswer
+        val numberOfTotalCard = tagList[position].totalNumberOfCard
+        val progressResult = (numberOfCorrectAnswer / numberOfTotalCard) * 100
+        holder.itemView.findViewById<ProgressBar>(R.id.cardRatioProgressBar).progress = progressResult
+
+        // ratio as a string
+        val ratioStr = "$numberOfCorrectAnswer $numberOfTotalCard"
+        holder.itemView.findViewById<TextView>(R.id.cardRatio).text = ratioStr
+    }
+
+    fun updateTagList(newTagList : List<Tag>) {
+        tagList.clear()
+        tagList.addAll(newTagList)
+        notifyDataSetChanged()
+    }
+}
