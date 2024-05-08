b9920ca934ede9c999a62fe59f05c476f2de260b
erenncetin <147340241+erenncetin@users.noreply.github.com>
2024-05-08 21:05:10 +0300

    temel eklendi

--- app/src/main/java/com/example/studyflow/view/pomodoroView/PomodoroFragment.kt
+++ app/src/main/java/com/example/studyflow/view/pomodoroView/PomodoroFragment.kt
@@ -5,17 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.studyflow.R
+import com.example.studyflow.databinding.FragmentPomodoroBinding
+import com.example.studyflow.databinding.FragmentToDoBinding
 import com.example.studyflow.interfaces.pomodoro.PomodoroFragmentClickListener
 import com.example.studyflow.viewmodel.BaseViewModel
 import com.example.studyflow.viewmodel.pomodoro.PomodoroViewModel
+import com.example.studyflow.viewmodel.todo.ToDoViewModel
 
 
 class PomodoroFragment : Fragment(), PomodoroFragmentClickListener {
+
     private lateinit var viewModel: PomodoroViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
     }
 
@@ -23,13 +32,49 @@ class PomodoroFragment : Fragment(), PomodoroFragmentClickListener {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pomodoro, container, false)
+        val binding: FragmentPomodoroBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_pomodoro, container, false
+        )
+
+        binding.listener = this
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this)[PomodoroViewModel::class.java]
+
+    }
+
+    private fun observeLiveData(view: View) {
+
+        if (viewModel.focusingMinutes.value != 0.toLong()) {
+            viewModel.focusingMinutes.observe(viewLifecycleOwner, Observer {
+                viewModel.countDownTime(view)
+            })
+        }
+        else if(viewModel.focusingSeconds.value != 0.toLong()) {
+            viewModel.focusingMinutes.observe(viewLifecycleOwner, Observer {
+                viewModel.countDownTime(view)
+            })
+        }
+
     }
 
     // click listener functions
     override fun onStart(view: View) {
         // create a pomodoro object and start it
+        // önce dakika ve saniyeyi atama yapmak lazım sonra
+        // ikisi de 00 mı değil mi onun kontorlu lazım
+        val minutes = view.findViewById<EditText>(R.id.Minutes).toString().toLong()
+        val seconds =  view.findViewById<EditText>(R.id.Seconds).toString().toLong()
+        if (!(minutes == 0.toLong() && seconds == 0.toLong())) {
+            viewModel.setMinuteAndSecond(minutes,seconds)
+            observeLiveData(view)
+        }
     }
 
     override fun onStop(view: View) {
--- app/src/main/java/com/example/studyflow/viewmodel/pomodoro/PomodoroViewModel.kt
+++ app/src/main/java/com/example/studyflow/viewmodel/pomodoro/PomodoroViewModel.kt
@@ -1,9 +1,56 @@
 package com.example.studyflow.viewmodel.pomodoro
 
 import android.app.Application
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.EditText
+import androidx.lifecycle.MutableLiveData
+import com.example.studyflow.R
 import com.example.studyflow.viewmodel.BaseViewModel
-
+import java.util.*
 open class PomodoroViewModel(application: Application) : BaseViewModel(application) {
 
+    val focusingMinutes = MutableLiveData<Long>()
+    val focusingSeconds = MutableLiveData<Long>()
+    val calendarStart = MutableLiveData<Calendar>()
+    val calendarEnd = MutableLiveData<Calendar>()
+
+    // Dakika ve Saniyeleri ilk başta değer atıcaz
+    fun setMinuteAndSecond(minutes: Long, seconds: Long) {
+        focusingMinutes.value = minutes
+        focusingSeconds.value = seconds
+    }
+
+    // Verilen süreyi geri sayma işlemi burada yapılacak
+    fun countDownTime(view: View) {
+
+        // Hepsini milisecond türünden tutuyorum
+        var totalMin = focusingMinutes.value!!.times(60000)
+        var totalSec = focusingSeconds.value!!.times(1000)
+        var totalTime = totalMin + totalSec
+
+        // Her starta basıldığında countDownTime çalışmalı
+        // burada da database tablosundaki start columnu için Calendar objesi oluşturuluyor ilk kez
+        if (calendarStart.value == null) {
+            calendarStart.value = Calendar.getInstance()
+        }
+
+        object: CountDownTimer(totalTime,1000) {
+
+            // Her bir arrivale ulaştığında güncelle
+            override fun onTick(millisUntilFinished: Long) {
+                view.findViewById<EditText>(R.id.Minutes).setText((totalTime / 60000).toString())
+                view.findViewById<EditText>(R.id.Seconds).setText((totalTime % 60000).toString())
+            }
+
+            override fun onFinish() {
+                // Toplam süre bittiğinde de obje initialize edilip
+                // focus kısma geçilmeli
+                calendarEnd.value = Calendar.getInstance()
+                println("The operation is done")
+            }
+
+        }
+    }
 
 }
