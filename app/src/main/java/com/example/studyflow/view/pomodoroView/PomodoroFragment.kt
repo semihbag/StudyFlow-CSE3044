00e3e7a325d07fd7fb4a99633f6f76df6bbab883
Hakkı Kokur <hakkikokur24@gmail.com>
2024-05-08 18:07:28 +0300

    pomodoro viewmodel and click listener added

--- app/src/main/java/com/example/studyflow/view/pomodoroView/PomodoroFragment.kt
+++ app/src/main/java/com/example/studyflow/view/pomodoroView/PomodoroFragment.kt
@@ -6,9 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.studyflow.R
+import com.example.studyflow.interfaces.pomodoro.PomodoroFragmentClickListener
+import com.example.studyflow.viewmodel.BaseViewModel
+import com.example.studyflow.viewmodel.pomodoro.PomodoroViewModel
 
 
-class PomodoroFragment : Fragment() {
+class PomodoroFragment : Fragment(), PomodoroFragmentClickListener {
+    private lateinit var viewModel: PomodoroViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,4 +27,17 @@ class PomodoroFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_pomodoro, container, false)
     }
 
+    // click listener functions
+    override fun onStart(view: View) {
+        // create a pomodoro object and start it
+    }
+
+    override fun onStop(view: View) {
+    }
+
+    override fun onPause(view: View) {
+    }
+
+    override fun onResume(view: View) {
+    }
 }
---
+++ app/src/main/java/com/example/studyflow/viewmodel/pomodoro/PomodoroViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.studyflow.viewmodel.pomodoro
+
+import android.app.Application
+import com.example.studyflow.viewmodel.BaseViewModel
+
+open class PomodoroViewModel(application: Application) : BaseViewModel(application) {
+
+
+}
