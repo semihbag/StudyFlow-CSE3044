5a99b166364902246d81a2ae21d132d2d71eaa18
Hakkı Kokur <hakkikokur24@gmail.com>
2024-05-09 20:50:02 +0300

    pause and resume done

--- app/src/main/java/com/example/studyflow/view/pomodoroView/PomodoroFragment.kt
+++ app/src/main/java/com/example/studyflow/view/pomodoroView/PomodoroFragment.kt
@@ -1,10 +1,12 @@
 package com.example.studyflow.view.pomodoroView
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
@@ -21,8 +23,15 @@ import com.example.studyflow.viewmodel.todo.ToDoViewModel
 class PomodoroFragment : Fragment(), PomodoroFragmentClickListener {
 
     private lateinit var viewModel: PomodoroViewModel
+    // UI
     private lateinit var minutes: EditText
     private lateinit var seconds: EditText
+    private lateinit var startButton: Button
+    private lateinit var stopButton: Button
+    private lateinit var pauseButton: Button
+    private lateinit var resumeButton: Button
+    // counter
+    private lateinit var counter: CountDownTimer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,19 +59,10 @@ class PomodoroFragment : Fragment(), PomodoroFragmentClickListener {
         viewModel = ViewModelProvider(this)[PomodoroViewModel::class.java]
         minutes = view.findViewById(R.id.Minutes)
         seconds = view.findViewById(R.id.Seconds)
-
-
-    }
-
-    private fun observeLiveData(view: View) {
-        if (viewModel.focusingMinutes.value != null && viewModel.focusingMinutes.value != 0L) {
-            viewModel.focusingMinutes.observe(viewLifecycleOwner, Observer {
-            })
-        }
-        else if(viewModel.focusingSeconds.value != null && viewModel.focusingSeconds.value != 0L) {
-            viewModel.focusingSeconds.observe(viewLifecycleOwner, Observer {
-            })
-        }
+        startButton = view.findViewById(R.id.startButton)
+        stopButton = view.findViewById(R.id.stopButton)
+        pauseButton = view.findViewById(R.id.pauseButton)
+        resumeButton = view.findViewById(R.id.resmuseButton)
 
     }
 
@@ -72,6 +72,10 @@ class PomodoroFragment : Fragment(), PomodoroFragmentClickListener {
         minutes.isEnabled = false
         seconds.isEnabled = false
 
+        // change the button
+        startButton.visibility = View.GONE
+        stopButton.visibility = View.VISIBLE
+
         // create a pomodoro object and start it
         // önce dakika ve saniyeyi atama yapmak lazım sonra
         // ikisi de 00 mı değil mi onun kontorlu lazım
@@ -79,7 +83,8 @@ class PomodoroFragment : Fragment(), PomodoroFragmentClickListener {
         val second = seconds.text.toString().toLong()
         if (!(minute == 0L && second == 0L)) {
             viewModel.setMinuteAndSecond(minute, second)
-            viewModel.countDownTime(minutes,seconds)
+            counter = viewModel.countDownTime(minutes,seconds) // returning counter object
+            counter.start()
         }
     }
 
@@ -87,10 +92,22 @@ class PomodoroFragment : Fragment(), PomodoroFragmentClickListener {
 
     }
 
-
     override fun onPause(view: View) {
+        // change the buttons (stop and resume will be visible)
+        if (!(!::counter.isInitialized)){
+            resumeButton.visibility = View.VISIBLE
+            pauseButton.visibility = View.GONE
+            // stop the counter
+            counter.cancel()
+        }
     }
 
     override fun onResume(view: View) {
+        // change the buttons (stop and resume will be visible)
+        resumeButton.visibility = View.GONE
+        pauseButton.visibility = View.VISIBLE
+        // stop the counter
+        counter = viewModel.countDownTime(minutes,seconds)
+        counter.start()
     }
 }
--- app/src/main/java/com/example/studyflow/viewmodel/pomodoro/PomodoroViewModel.kt
+++ app/src/main/java/com/example/studyflow/viewmodel/pomodoro/PomodoroViewModel.kt
@@ -10,36 +10,37 @@ import com.example.studyflow.viewmodel.BaseViewModel
 import java.util.*
 open class PomodoroViewModel(application: Application) : BaseViewModel(application) {
 
+    // pomodoro variables
     val focusingMinutes = MutableLiveData<Long>()
     val focusingSeconds = MutableLiveData<Long>()
     val calendarStart = MutableLiveData<Calendar>()
     val calendarEnd = MutableLiveData<Calendar>()
 
+    // counter variables
+    val totalTimeInMilsec = MutableLiveData<Long>()
+    val remaingTimeInMilsec = MutableLiveData<Long>()
+
     // Dakika ve Saniyeleri ilk başta değer atıcaz
     fun setMinuteAndSecond(minutes: Long, seconds: Long) {
         focusingMinutes.value = minutes
         focusingSeconds.value = seconds
+        totalTimeInMilsec.value = (minutes * 60000) + (seconds * 1000)
+        remaingTimeInMilsec.value = totalTimeInMilsec.value
     }
 
     // Verilen süreyi geri sayma işlemi burada yapılacak
-    fun countDownTime(minutesEditText: EditText, secondsEditText: EditText) {
-
-        // Hepsini milisecond türünden tutuyorum
-        var totalMin = focusingMinutes.value!!.times(60000)
-        var totalSec = focusingSeconds.value!!.times(1000)
-        var totalTime = totalMin + totalSec
-        // Her starta basıldığında countDownTime çalışmalı
+    fun countDownTime(minutesEditText: EditText, secondsEditText: EditText): CountDownTimer {
         // burada da database tablosundaki start columnu için Calendar objesi oluşturuluyor ilk kez
         if (calendarStart.value == null) {
             // Anlık zamanı milisaniye türünde alma
-            val currentTimeInMill = System.currentTimeMillis()
             val calendarObject = Calendar.getInstance()
-            calendarObject.timeInMillis = currentTimeInMill
+            calendarObject.timeInMillis = System.currentTimeMillis()
             calendarStart.value = calendarObject
         }
 
-        object : CountDownTimer(totalTime,1000) {
+        val counter =  object : CountDownTimer(remaingTimeInMilsec.value!!, 1000L) {
             override fun onTick(millisUntilFinished: Long) {
+                remaingTimeInMilsec.value = remaingTimeInMilsec.value?.minus(1000L)
                 minutesEditText.setText((millisUntilFinished / 60000).toString())
                 secondsEditText.setText(((millisUntilFinished % 60000) / 1000).toString())
             }
@@ -47,14 +48,14 @@ open class PomodoroViewModel(application: Application) : BaseViewModel(applicati
             override fun onFinish() {
                 // Toplam süre bittiğinde de obje initialize edilip
                 // focus kısma geçilmeli
-                val currentTimeInMill = System.currentTimeMillis()
                 val calendarObject = Calendar.getInstance()
-                calendarObject.timeInMillis = currentTimeInMill
+                calendarObject.timeInMillis = System.currentTimeMillis()
                 calendarEnd.value = calendarObject
 
             }
 
-        }.start()
+        }
+        return counter
 
     }
 
