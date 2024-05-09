b55c49885db9330a66271089039db5315bb9c1b7
erenncetin <147340241+erenncetin@users.noreply.github.com>
2024-05-09 23:31:06 +0300

    Bug fixed

--- app/src/main/java/com/example/studyflow/model/Pomodoro.kt
+++ app/src/main/java/com/example/studyflow/model/Pomodoro.kt
@@ -3,6 +3,7 @@ package com.example.studyflow.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.UUID
 
 @Entity
 data class Pomodoro (
