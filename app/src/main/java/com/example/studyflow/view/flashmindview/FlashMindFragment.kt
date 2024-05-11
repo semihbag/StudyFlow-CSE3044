aeaf529799b3693d965f5672dbffe11efa4f8a85
semihbag <semih702bag@gmail.com>
2024-05-11 16:39:06 +0300

    swipe with colors

--- app/src/main/java/com/example/studyflow/view/flashmindview/FlashMindFragment.kt
+++ app/src/main/java/com/example/studyflow/view/flashmindview/FlashMindFragment.kt
@@ -1,5 +1,9 @@
 package com.example.studyflow.view.flashmindview
 
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -56,9 +60,45 @@ class FlashMindFragment : Fragment() {
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
                 if (direction == ItemTouchHelper.RIGHT) {
-                    // Sağa kaydırma işlemi için yapılacak işlemler
+                        // sağa kaydım
                 } else if (direction == ItemTouchHelper.LEFT) {
-                    // Sola kaydırma işlemi için yapılacak işlemler
+                    // sola kaydım
+                }
+            }
+
+            override fun onChildDraw(c: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean) {
+                if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+                    super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+
+                    // right background
+                    val backgroundRight = ColorDrawable(Color.BLUE)
+                    backgroundRight.setBounds(viewHolder.itemView.right + dX.toInt(), viewHolder.itemView.top, viewHolder.itemView.right, viewHolder.itemView.bottom)
+                    backgroundRight.draw(c)
+
+                    // Exercise text
+                    val textPaintRight = Paint().apply {
+                        color = Color.WHITE
+                        textSize = 40f
+                        textAlign = Paint.Align.CENTER
+                    }
+                    val textRight = "EXERCISE"
+                    c.drawText(textRight, viewHolder.itemView.right.toFloat() + dX / 2, viewHolder.itemView.bottom.toFloat() - viewHolder.itemView.height / 2, textPaintRight)
+
+
+                    // left background
+                    val backgroundLeft = ColorDrawable(Color.RED)
+                    backgroundLeft.setBounds(viewHolder.itemView.left + dX.toInt(), viewHolder.itemView.top, viewHolder.itemView.left, viewHolder.itemView.bottom)
+                    backgroundLeft.draw(c)
+
+                    // Exercise text
+                    val textPaintLeft = Paint().apply {
+                        color = Color.WHITE
+                        textSize = 40f
+                        textAlign = Paint.Align.CENTER
+                    }
+                    val textLeft = "MY CARDS"
+                    c.drawText(textLeft, viewHolder.itemView.left.toFloat() + dX / 2, viewHolder.itemView.bottom.toFloat() - viewHolder.itemView.height / 2, textPaintLeft)
+
                 }
             }
         })
