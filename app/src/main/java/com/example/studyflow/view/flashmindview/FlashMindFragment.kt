243e29e328a9a985b8cb6ebe675e917669e05e50
semihbag <semih702bag@gmail.com>
2024-05-11 16:11:03 +0300

    swipe the tag (flash mind)

--- app/src/main/java/com/example/studyflow/view/flashmindview/FlashMindFragment.kt
+++ app/src/main/java/com/example/studyflow/view/flashmindview/FlashMindFragment.kt
@@ -8,6 +8,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.studyflow.R
@@ -48,6 +49,20 @@ class FlashMindFragment : Fragment() {
         recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.adapter = recyclerAdapter
 
+        val itemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                if (direction == ItemTouchHelper.RIGHT) {
+                    // Sağa kaydırma işlemi için yapılacak işlemler
+                } else if (direction == ItemTouchHelper.LEFT) {
+                    // Sola kaydırma işlemi için yapılacak işlemler
+                }
+            }
+        })
+        itemTouchHelper.attachToRecyclerView(recyclerView)
         observeLiveData()
     }
 
