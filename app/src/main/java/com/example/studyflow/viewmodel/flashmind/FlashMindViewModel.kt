605f42245bd85f41e9f1b48c3d2f615056dce772
semihbag <semih702bag@gmail.com>
2024-05-11 15:45:36 +0300

    Update FlashMindViewModel.kt

--- app/src/main/java/com/example/studyflow/viewmodel/flashmind/FlashMindViewModel.kt
+++ app/src/main/java/com/example/studyflow/viewmodel/flashmind/FlashMindViewModel.kt
@@ -1,7 +1,25 @@
 package com.example.studyflow.viewmodel.flashmind
 
 import android.app.Application
+import androidx.lifecycle.MutableLiveData
+import com.example.studyflow.model.Tag
+import com.example.studyflow.service.StudyFlowDB
 import com.example.studyflow.viewmodel.BaseViewModel
+import kotlinx.coroutines.launch
 
 class FlashMindViewModel(application: Application) : BaseViewModel(application) {
+    private val dB = StudyFlowDB(getApplication())
+    private val daoTag = dB.tagDao()
+    private val daoToDo = dB.toDoDao()
+
+    val mutableFlashMindTags = MutableLiveData<List<Tag>>()
+
+    fun loadFlashMindTagsFromDB() {
+
+        launch {
+            val currentTags = daoTag.getAllTag()
+            mutableFlashMindTags.value = currentTags
+        }
+        
+    }
 }
