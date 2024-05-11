39ae493f578b3f4ae70ad5e9a2cd2a94903a615a
semihbag <semih702bag@gmail.com>
2024-05-12 01:41:45 +0300

    Create Card.kt

---
+++ app/src/main/java/com/example/studyflow/model/Card.kt
@@ -0,0 +1,32 @@
+package com.example.studyflow.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Card(
+    @ColumnInfo("cardTitle")
+    val cardTitle : String,
+    @ColumnInfo("createDate")
+    val createDate : Long,
+    @ColumnInfo("textFront")
+    val textFront : String?,
+    @ColumnInfo("textBack")
+    val textBack : String?,
+    @ColumnInfo("imagePathFront")
+    val imagePathFront : String?,
+    @ColumnInfo("imagePathBack")
+    val imagePathBack : String?,
+    @ColumnInfo("isMarked")
+    val isMarked : Boolean = true,
+    @ColumnInfo("state")
+    val state : Int = 1,
+    @ColumnInfo("lastExerciseDate")
+    val lastExerciseDate : Long,
+    @ColumnInfo("lastAnswer")
+    val lastAnswer : Boolean
+) {
+    @PrimaryKey(true)
+    var uuid : Int = 0
+}
