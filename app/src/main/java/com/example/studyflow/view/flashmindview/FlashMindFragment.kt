1ca83ac80f2f2bd6aeda45032a1203912c6951a4
semihbag <semih702bag@gmail.com>
2024-05-11 15:41:59 +0300

    Update FlashMindFragment.kt

--- app/src/main/java/com/example/studyflow/view/flashmindview/FlashMindFragment.kt
+++ app/src/main/java/com/example/studyflow/view/flashmindview/FlashMindFragment.kt
@@ -4,20 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.studyflow.R
+import com.example.studyflow.adapter.flasmind.FlashMindTagRecyclerAdapter
+import com.example.studyflow.databinding.FragmentFlashMindBinding
+import com.example.studyflow.model.Tag
+import com.example.studyflow.viewmodel.flashmind.FlashMindViewModel
+import com.example.studyflow.viewmodel.tag.TagViewModel
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FlashMindFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FlashMindFragment : Fragment() {
-
+    private lateinit var viewModel : FlashMindViewModel
+    private val recyclerAdapter = FlashMindTagRecyclerAdapter(ArrayList<Tag>())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-
         }
     }
 
@@ -25,9 +29,27 @@ class FlashMindFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_flash_mind, container, false)
+        val binding : FragmentFlashMindBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_flash_mind, container,false
+        )
+
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(FlashMindViewModel::class.java)
+
+        // burda db load fonk gelcek
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.flash_mind_tag_row_recyclerview)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = recyclerAdapter
+
+        
+
+    }
 
 }
