b241749a51bf52b7c67ebb9d9dd16355b5bc3b53
semihbag <semih702bag@gmail.com>
2024-05-13 22:42:22 +0300

    Update StudyFlowDB.kt

--- app/src/main/java/com/example/studyflow/service/StudyFlowDB.kt
+++ app/src/main/java/com/example/studyflow/service/StudyFlowDB.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.studyflow.model.Card
 import com.example.studyflow.model.Tag
 import com.example.studyflow.model.ToDo
 
@@ -25,7 +26,7 @@ import com.example.studyflow.model.ToDo
     DAO YU DA EKLEMEYİ UNUTMAYIN
 
 */
-@Database(entities = [Tag::class, ToDo::class], version = 8)
+@Database(entities = [Tag::class, ToDo::class, Card::class], version = 9)
 abstract class StudyFlowDB : RoomDatabase() {
     abstract fun tagDao() : TagDAO
     abstract fun toDoDao() : ToDoDAO
