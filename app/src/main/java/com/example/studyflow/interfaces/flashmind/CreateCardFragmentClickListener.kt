d764be4912cdb3ae173d52bf44432e7004301a13
semihbag <semih702bag@gmail.com>
2024-05-14 14:16:05 +0300

    Create CreateCardFragmentClickListener.kt

---
+++ app/src/main/java/com/example/studyflow/interfaces/flashmind/CreateCardFragmentClickListener.kt
@@ -0,0 +1,11 @@
+package com.example.studyflow.interfaces.flashmind
+
+import android.view.View
+
+interface CreateCardFragmentClickListener {
+
+    fun clickEditCard(view: View)
+
+    fun clickCreateCard(view: View)
+    
+}
