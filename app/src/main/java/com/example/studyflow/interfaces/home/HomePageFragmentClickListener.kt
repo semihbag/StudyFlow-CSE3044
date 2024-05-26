54b3437b68f4bfb770fe81131324524fa0b5d78e
semihbag <semih702bag@gmail.com>
2024-05-26 23:03:31 +0300

    BIG HOMEPAGE UI DESIGN UPDATE

---
+++ app/src/main/java/com/example/studyflow/interfaces/home/HomePageFragmentClickListener.kt
@@ -0,0 +1,23 @@
+package com.example.studyflow.interfaces.home
+
+import android.view.View
+
+interface HomePageFragmentClickListener {
+
+    fun clickGoFocusPage(view : View)
+
+
+    fun clickGoFlashMindPage(view : View)
+
+
+    fun clickGoAnalysisPage(view : View)
+
+
+    fun clickGoPlanningPage(view : View)
+
+
+    fun clickGoTodoPage(view : View)
+
+
+    fun clickGoTagsPage(view : View)
+}
