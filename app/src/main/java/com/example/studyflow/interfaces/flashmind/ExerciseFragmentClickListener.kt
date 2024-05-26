6f0883d363e20b22a7e7e178b4a54014ff97e800
semihbag <semih702bag@gmail.com>
2024-05-27 02:25:01 +0300

    Create ExerciseFragmentClickListener.kt

---
+++ app/src/main/java/com/example/studyflow/interfaces/flashmind/ExerciseFragmentClickListener.kt
@@ -0,0 +1,20 @@
+package com.example.studyflow.interfaces.flashmind
+
+import android.view.View
+
+interface ExerciseFragmentClickListener {
+
+    fun clickBack(view: View)
+
+
+    fun clickFalse(view: View)
+
+
+    fun clickFlip(view: View)
+
+
+    fun clickTrue(view: View)
+
+
+    fun clickPass(view: View)
+}
