794c92d982a8c72492fe09d2e48c1fe3df7e96ed
semihbag <semih702bag@gmail.com>
2024-05-27 02:37:36 +0300

    Update ExerciseFragment.kt

--- app/src/main/java/com/example/studyflow/view/flashmindview/ExerciseFragment.kt
+++ app/src/main/java/com/example/studyflow/view/flashmindview/ExerciseFragment.kt
@@ -5,15 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.studyflow.R
+import com.example.studyflow.databinding.FragmentExerciseBinding
+import com.example.studyflow.interfaces.flashmind.ExerciseFragmentClickListener
+import com.example.studyflow.model.Tag
+import com.example.studyflow.viewmodel.flashmind.ExerciseViewModel
 
-class ExerciseFragment : Fragment() {
-
+class ExerciseFragment : Fragment(), ExerciseFragmentClickListener {
+    private lateinit var viewModel : ExerciseViewModel
+    private lateinit var tag : Tag
+    private lateinit var binding : FragmentExerciseBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-
+            tag = ExerciseFragmentArgs.fromBundle(it).tag
         }
     }
 
@@ -21,8 +29,49 @@ class ExerciseFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exercise, container, false)
+        binding = DataBindingUtil.inflate((inflater, R.layout.fragment_exercise, container, false)
+
+        binding.listener = this
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(ExerciseViewModel::class.java)
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    override fun clickBack(view: View) {
+        TODO("Not yet implemented")
+    }
+
+    override fun clickFalse(view: View) {
+        TODO("Not yet implemented")
+    }
+
+    override fun clickFlip(view: View) {
+        TODO("Not yet implemented")
+    }
+
+    override fun clickTrue(view: View) {
+        TODO("Not yet implemented")
+    }
+
+    override fun clickPass(view: View) {
+        TODO("Not yet implemented")
     }
 
 
