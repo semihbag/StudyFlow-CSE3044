af72b4b0593759601bbf248ab80dce90c9d093ed
erenncetin <147340241+erenncetin@users.noreply.github.com>
2024-05-27 03:33:55 +0300

    DataBase veri çekiyor

--- app/src/main/java/com/example/studyflow/view/analysisview/AnalysisFragment.kt
+++ app/src/main/java/com/example/studyflow/view/analysisview/AnalysisFragment.kt
@@ -4,22 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.studyflow.R
 import com.example.studyflow.databinding.FragmentAnalysisBinding
 import com.example.studyflow.databinding.FragmentPomodoroBinding
 import com.example.studyflow.databinding.TagBottomSheetDialogRowBinding
 import com.example.studyflow.interfaces.analysis.AnalysisFragmentClickListener
 import com.example.studyflow.interfaces.tag.TagBottomSheetDialogClickListener
+import com.example.studyflow.model.Pomodoro
 import com.example.studyflow.view.tagview.TagBottomSheetDialogFragment
+import com.example.studyflow.viewmodel.analysis.AnalysisViewModel
 import com.example.studyflow.viewmodel.pomodoro.PomodoroViewModel
 
 class AnalysisFragment : Fragment(), TagBottomSheetDialogClickListener, AnalysisFragmentClickListener {
 
 
-    // private lateinit var viewModel: Analysis
+    private lateinit var viewModel: AnalysisViewModel
     private lateinit var tagBottomSheetDialogFragment: TagBottomSheetDialogFragment
+    private var allPomodorosOfTag: List<Pomodoro> = mutableListOf()
 
     private lateinit var selectedTag: com.example.studyflow.model.Tag
     private var tagID = -1
@@ -41,9 +47,34 @@ class AnalysisFragment : Fragment(), TagBottomSheetDialogClickListener, Analysis
         return binding.root
     }
 
+    fun observeLiveData() {
+        viewModel.pomodoroItems.observe(viewLifecycleOwner, Observer { pomodoros ->
+            allPomodorosOfTag = pomodoros
+            calculateTotalTime()
+        })
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this)[AnalysisViewModel::class.java]
         tagBottomSheetDialogFragment = TagBottomSheetDialogFragment(this)
+    }
+
+
+    private fun calculateTotalTime() {
+        var totalTime: Long = 0
+        for (pomodoro in allPomodorosOfTag) {
+            totalTime += (pomodoro.endTime - pomodoro.startTime)
+        }
+
+        view?.let {
+            val binding = DataBindingUtil.findBinding<FragmentAnalysisBinding>(it)
+            binding?.let { bind ->
+                bind.TotalMinText.text = (totalTime / 60000).toString()
+                bind.TotalSecText.text = (totalTime % 60000 / 1000).toString()
+            }
+        }
     }
 
     override fun onTag(view: View) {
@@ -69,5 +100,9 @@ class AnalysisFragment : Fragment(), TagBottomSheetDialogClickListener, Analysis
             }
         }
         tagBottomSheetDialogFragment.dismiss()
+        if (tagID != -1) {
+            viewModel.findAllPomodorosOfSpesificTag(tagID)
+            observeLiveData()
+        }
     }
 }
